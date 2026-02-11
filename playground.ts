56d7c094280dda8a6fede4cc631b5453d593f549
Saad Ali <155216987+itsmesaadali@users.noreply.github.com>
2026-02-11 12:28:09 +0500

    feat: add playground script and update Prisma client configuration

---
+++ playground.ts
@@ -0,0 +1,16 @@
+// import "dotenv/config";
+
+// import { db } from "@/server/db";
+
+
+// const user=  await db.user.create({
+//     data:{
+//         email: "itmesaad@gmail.com",
+//         firstName:"Saad",
+//         lastName:"Ali"
+//     }
+// })
+
+// const user = await db.user.findMany();
+
+// console.log("User add in db successfully",user);
