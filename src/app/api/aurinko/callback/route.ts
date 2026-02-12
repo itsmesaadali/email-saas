daaaa727ea595c87a51a646cfd9519fef0249b67
Saad Ali <155216987+itsmesaadali@users.noreply.github.com>
2026-02-12 09:21:42 +0500

    feat: add Aurinko authentication flow and link account button

---
+++ src/app/api/aurinko/callback/route.ts
@@ -0,0 +1,51 @@
+// /api/aurinko/callback
+
+import { exchangeCodeForAccessTokens, getAccountDetails } from "@/lib/aurinko";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server"
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+    const { userId } = await auth();
+    if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const params = req.nextUrl.searchParams;
+    const status = params.get("status");
+    if (status != "success") {
+        return new NextResponse("Callback failed", { status: 400 });
+    }
+
+    // get the code to excahnge for access tokens
+    const code = params.get("code");
+    if (!code) {
+        return new NextResponse("Missing code", { status: 400 });
+    }
+
+    const token = await exchangeCodeForAccessTokens(code);
+    if (!token) {
+        return new NextResponse("Failed to exchange code for access tokens", { status: 500 });
+    }
+
+    const accountDetails = await getAccountDetails(token.accessToken);
+
+    await db.account.upsert(
+        {
+            where: {
+                id: token.accountId.toString()
+            },
+            update: {
+                accessToken: token.accessToken
+            },
+            create: {
+                id: token.accountId.toString(),
+                userId,
+                emailAddress: accountDetails.email,
+                name: accountDetails.name,
+                accessToken: token.accessToken
+            }
+        }
+    )
+
+    return NextResponse.redirect(new URL("/mail", req.url));
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { ComponentExample } from "@/components/component-example";
+import { LinkAccountButton } from "@/components/link-account-button";
 
 export default function Page() {
   return (
     <div className="space-y-4 p-6">
+      <LinkAccountButton/>
  
 
       <ComponentExample />
